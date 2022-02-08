287039b6f14dd25bbfcae39e7bb4a502c230278e
grgndrd <greeg.andradee@gmail.com>
2022-02-08 14:19:11 +0000

    done

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  }
+  get(pos) {
+    if (this.items.indexOf(pos) === -1) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  add(item) {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length-1]
+    }
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0];
+    }
+  }
 
   sum() {}
 
